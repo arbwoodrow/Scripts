d318238513cba5c81ef1cd327930f1b8f2e09211
Alex Woodrow <alexander.woodrow@pearson.com>
2020-05-04 16:32:58 +0100

    Script to send a Wake On LAN packet to target MAC

---
+++ powershell/WakeOnLAN.ps1
@@ -0,0 +1,7 @@
+﻿$Mac = Read-Host "Please enter the MAC address of the machine and press Enter"
+$MacByteArray = $Mac -split "[:-]" | ForEach-Object { [Byte] "0x$_"}
+[Byte[]] $MagicPacket = (,0xFF * 6) + ($MacByteArray  * 16)
+$UdpClient = New-Object System.Net.Sockets.UdpClient
+$UdpClient.Connect(([System.Net.IPAddress]::Broadcast),7)
+$UdpClient.Send($MagicPacket,$MagicPacket.Length)
+$UdpClient.Close()
